a63e8ebcd9f24643bc129b9f6129c1ca8c08408e
kakenx <kakenv@hotmail.com>
2016-01-05 07:49:19 +0000

    Header, footer

---
+++ wp-content/themes/quark/footer.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id #maincontentcontainer div and all content after.
+ * There are also four footer widgets displayed. These will be displayed from
+ * one to four columns, depending on how many widgets are active.
+ *
+ * @package Quark
+ * @since Quark 1.0
+ */
+?>
+
+		<?php	do_action( 'quark_after_woocommerce' ); ?>
+	</div> <!-- /#maincontentcontainer -->
+
+	<div id="footercontainer">
+
+		<footer class="site-footer row" role="contentinfo">
+
+			<?php
+			// Count how many footer sidebars are active so we can work out how many containers we need
+			$footerSidebars = 0;
+			for ( $x=1; $x<=4; $x++ ) {
+				if ( is_active_sidebar( 'sidebar-footer' . $x ) ) {
+					$footerSidebars++;
+				}
+			}
+
+			// If there's one or more one active sidebars, create a row and add them
+			if ( $footerSidebars > 0 ) { ?>
+				<?php
+				// Work out the container class name based on the number of active footer sidebars
+				$containerClass = "grid_" . 12 / $footerSidebars . "_of_12";
+
+				// Display the active footer sidebars
+				for ( $x=1; $x<=4; $x++ ) {
+					if ( is_active_sidebar( 'sidebar-footer'. $x ) ) { ?>
+						<div class="col <?php echo $containerClass?>">
+							<div class="widget-area" role="complementary">
+								<?php dynamic_sidebar( 'sidebar-footer'. $x ); ?>
+							</div>
+						</div> <!-- /.col.<?php echo $containerClass?> -->
+					<?php }
+				} ?>
+
+			<?php } ?>
+
+		</footer> <!-- /.site-footer.row -->
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="col grid_8_of_12">
+					<div class="logo-footer">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home">
+								<?php 
+								$headerImg = get_header_image();
+								if( !empty( $headerImg ) ) { ?>
+									<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+								<?php } 
+								 ?>
+							</a>
+
+					</div>
+					<div class="menu-footer">
+						<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'footer-menu' ) ); ?>
+
+					</div>
+
+				</div>
+				<div class="col grid_4_of_12">
+					<div class="social-media-icons  social-footer">
+						<?php echo quark_get_social_media(); ?>
+					</div>
+					<?php if ( of_get_option( 'footer_content', quark_get_credits() ) ) {
+						echo '<div class="row smallprint">';
+						echo apply_filters( 'meta_content', wp_kses_post( of_get_option( 'footer_content', quark_get_credits() ) ) );
+						echo '</div> <!-- /.smallprint -->';
+					} ?>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+		
+
+	</div> <!-- /.footercontainer -->
+
+</div> <!-- /.#wrapper.hfeed.site -->
+
+<?php wp_footer(); ?>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-1.8.3.min.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+	    $('.close-miss').click(function(){
+	      $('.dont-miss').fadeOut(1000);
+	    });
+	    function date(){
+	    var currentTime = new Date()
+		var hours = currentTime.getHours()
+		var minutes = currentTime.getMinutes()
+		if (minutes < 10){
+		minutes = "0" + minutes
+		}
+		document.write(hours + ":" + minutes + " ")
+		if(hours > 11){
+		document.write("PM")
+		} else {
+		document.write("AM")
+		}
+		}
+    });
+	
+	
+
+
+</script>
+</body>
+
+</html>
