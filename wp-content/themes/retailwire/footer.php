ba1e7ea9ae41e3a1fcda08fa9abefdb02e8d8399
OneTimeUser <jason@thewondermob.com>
2016-02-14 18:54:05 -0500

    css fix and ad placement

--- wp-content/themes/retailwire/footer.php
+++ wp-content/themes/retailwire/footer.php
@@ -18,34 +18,7 @@
 
 		<footer class="site-footer row" role="contentinfo">
 
-			<?php
-			// Count how many footer sidebars are active so we can work out how many containers we need
-			$footerSidebars = 0;
-			for ( $x=1; $x<=4; $x++ ) {
-				if ( is_active_sidebar( 'sidebar-footer' . $x ) ) {
-					$footerSidebars++;
-				}
-			}
-
-			// If there's one or more one active sidebars, create a row and add them
-			if ( $footerSidebars > 0 ) { ?>
-				<?php
-				// Work out the container class name based on the number of active footer sidebars
-				$containerClass = "grid_" . 12 / $footerSidebars . "_of_12";
-
-				// Display the active footer sidebars
-				for ( $x=1; $x<=4; $x++ ) {
-					if ( is_active_sidebar( 'sidebar-footer'. $x ) ) { ?>
-						<div class="col <?php echo $containerClass?>">
-							<div class="widget-area" role="complementary">
-								<?php dynamic_sidebar( 'sidebar-footer'. $x ); ?>
-							</div>
-						</div> <!-- /.col.<?php echo $containerClass?> -->
-					<?php }
-				} ?>
-
-			<?php } ?>
-
+			
 		</footer> <!-- /.site-footer.row -->
 		<div class="footer-bottom">
 			<div class="container">
--- wp-content/themes/retailwire/page-home.php
+++ wp-content/themes/retailwire/page-home.php
@@ -264,18 +264,34 @@ get_header(); ?>
 			</div>
 			<div class="sec-3-r">
 				<div class="space">
-					<div class="ad-2">
-						<?php $background = of_get_option( 'ad_300x250', $background_defaults ); ?>
-						<img src="<?php echo esc_url( $background['image'] );  ?>">
-					</div>
-					<div class="ad-2">
-						<?php $background = of_get_option( 'ad-300x250', $background_defaults ); ?>
-						<img src="<?php echo esc_url( $background['image'] );  ?>">
-					</div>
-					<div class="ad-2">
-						<?php $background = of_get_option( 'ad300x250', $background_defaults ); ?>
-						<img src="<?php echo esc_url( $background['image'] );  ?>">
-					</div>
+					<?php
+			// Count how many footer sidebars are active so we can work out how many containers we need
+			$footerSidebars = 0;
+			for ( $x=1; $x<=4; $x++ ) {
+				if ( is_active_sidebar( 'sidebar-footer' . $x ) ) {
+					$footerSidebars++;
+				}
+			}
+
+			// If there's one or more one active sidebars, create a row and add them
+			if ( $footerSidebars > 0 ) { ?>
+				<?php
+				// Work out the container class name based on the number of active footer sidebars
+				//$containerClass = "grid_" . 12 / $footerSidebars . "_of_12";
+
+				// Display the active footer sidebars
+				for ( $x=1; $x<=4; $x++ ) {
+					if ( is_active_sidebar( 'sidebar-footer'. $x ) ) { ?>
+						<!--<div class="col <?php echo $containerClass?>">-->
+							<div class="widget-area" role="complementary">
+								<?php dynamic_sidebar( 'sidebar-footer'. $x ); ?>
+							</div>
+						<!--</div>--> <!-- /.col.<?php echo $containerClass?> -->
+					<?php }
+				} ?>
+
+			<?php } ?>
+
 				</div>
 			</div>
 		</div>
