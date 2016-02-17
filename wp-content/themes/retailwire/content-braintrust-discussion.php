d2a1852cb8e1bfc0baeb7c56fc168f4035573ddc
OneTimeUser <jason@thewondermob.com>
2016-02-17 10:53:58 -0500

    cleaned up Braintrust ccode

--- wp-content/themes/retailwire/content-braintrust-discussion.php
+++ wp-content/themes/retailwire/content-braintrust-discussion.php
@@ -1,89 +1,99 @@
 
-		<?php $num_comment=get_comments_number();
-				              		if($num_comment > 0){ ?>
+									<?php 
+				              		if(get_field('feat_comm')){ ?>
 				              		<div class="item-braintrust">	
 									<span class="module-label title-brain">Braintrust</span>
-
-				              		<?php $args = array(
-									    'number' => 10000,
-									    'post_id' => $post->ID,
-									    'status' => 'approve'
-									);?>
-									<?php $latest_comment = get_comments($args);
-									if( $latest_comment ) foreach( $latest_comment as $comment ) { 
-										
-										$vote = get_comment_meta( $comment->comment_ID, 'age1', true );
-										//var_dump($vote);
-										if($vote=="on"){
-										?>
-									<div class="item-braintrust-sing-post">
+									
 										<div class="item-b-l">
 				              			
-										<?php $author_id = get_the_author_meta('ID');
-											  $content_user = get_field('content', 'user_'. $author_id );
+										<?php //$author_id = get_the_author_meta('ID');
+											  //$content_user = get_field('content', 'user_'. $author_id );
+											  $username = get_field('feat_brain');
+											  $user_id = $username['ID'];
 										 ?>
-										<div class="desc">"<?php echo wp_html_excerpt( $comment->comment_content, 100 ); ?>"</div>
+										<div class="desc">"<?php the_field('feat_comm'); ?>"</div>
 										</div>
 										<div class="item-b-r">
-											<a class="link-name-author" href="<?php echo get_author_posts_url($author_id); ?>"><h2><?php echo $comment->comment_author; ?>:</h2></a>
+											<a class="link-name-author" href="<?php echo get_author_posts_url($user_id); ?>"><h2><?php echo $username["display_name"]; ?></h2></a>
 											<div class="img-user">
-												<a href="<?php echo get_author_posts_url($author_id); ?>">
+												<a href="<?php echo get_author_posts_url($user_id); ?>">
 													<?php 
 													 $size="110";
-													 echo get_avatar($author_id,$size);
+													 echo get_avatar($user_id,$size);
 													 ?>
 												</a>
 											</div>
-											<a class="link-name-author show-600" href="<?php echo get_author_posts_url($author_id); ?>"><h2><?php echo get_the_author(); ?></h2></a>
+											<a class="link-name-author show-600" href="<?php echo get_author_posts_url($user_id); ?>"><h2><?php echo $username["display_name"]; ?></h2></a>
 										</div>
 										<div class="line-5"></div>
-									</div>
 									
+									<?php 
+				              		if(get_field('feat_comm_2')){ ?>
 
-									<?php }?> 
-
-									<?php if($vote==""){
-										?>
-									<div class="item-braintrust-sing-post">	
+									
 										<div class="item-b-l">
 					              			
-											<?php $author_id = get_the_author_meta('ID');
-												  $content_user = get_field('content', 'user_'. $author_id );
+											<?php $username2 = get_field('feat_brain_2');
+											  		$user_id2 = $username2['ID'];
 											 ?>
-										<div class="desc">"<?php echo wp_html_excerpt( $comment->comment_content, 100 ); ?>"</div>
-										</div>
-										<div class="item-b-r">
-											<a class="link-name-author" href="<?php echo get_author_posts_url($author_id); ?>"><h2><?php echo $comment->comment_author; ?>:</h2></a>
-											<div class="img-user">
-												<a href="<?php echo get_author_posts_url($author_id); ?>">
-													<?php 
-													 $size="110";
-													 echo get_avatar($author_id,$size);
-													 ?>
-												</a>
-											</div>
-											<a class="link-name-author show-600" href="<?php echo get_author_posts_url($author_id); ?>"><h2><?php echo get_the_author(); ?></h2></a>
-										</div>
-										<div class="line-5"></div>
+										<div class="desc">"<?php the_field('feat_comm_2'); ?>"</div>
 									</div>
-									<?php } ?>
-								<?php } ?>
-				              		
+									<div class="item-b-r">
+										<a class="link-name-author" href="<?php echo get_author_posts_url($user_id2); ?>"><h2><?php echo $username2["display_name"]; ?></h2></a>
+										<div class="img-user">
+											<a href="<?php echo get_author_posts_url($user_id2); ?>">
+												<?php 
+												 $size="110";
+												 echo get_avatar($user_id2,$size);
+												 ?>
+											</a>
+										</div>
+										<a class="link-name-author show-600" href="<?php echo get_author_posts_url($user_id2); ?>"><h2><?php echo $username2["display_name"]; ?></h2></a>
+									</div>
+										<div class="line-5"></div>
 									
-								</div>
+									<?php } 
+									if(get_field('feat_comm_3')){ ?>
+								
+										<div class="item-b-l">
+					              			
+											<?php $username3 = get_field('feat_brain_3');
+											  		$user_id3 = $username3['ID'];
+											 ?>
+										<div class="desc">"<?php the_field('feat_comm_3'); ?>"</div>
+									</div>
+									<div class="item-b-r">
+										<a class="link-name-author" href="<?php echo get_author_posts_url($user_id3); ?>"><h2><?php echo $username3["display_name"]; ?></h2></a>
+										<div class="img-user">
+											<a href="<?php echo get_author_posts_url($user_id3); ?>">
+												<?php 
+												 $size="110";
+												 echo get_avatar($user_id3,$size);
+												 ?>
+											</a>
+										</div>
+										<a class="link-name-author show-600" href="<?php echo get_author_posts_url($user_id3); ?>"><h2><?php echo $username3["display_name"]; ?></h2></a>
+									</div>
+										
+									
+				              		
+									</div>
+								
+								<?php } ?>
 								<?php	} else {  ?>
 
-								<div class="item-braintrust ">
+								<div class="item-braintrust">
 				              		<div class="item-b-l">
 				              			<span class="module-label title-brain">Braintrust</span>
-										<?php $author_id = get_the_author_meta('ID');
-											  $content_user = get_field('content', 'user_'. $author_id );
+										<?php //$author_id = get_the_author_meta('ID');
+											  //$content_user = get_field('content', 'user_'. $author_id );
 										 ?>
-									<div class="desc">
-									<?php echo $content_user; ?> 
+									<div class="desc"><!--
+									<?php echo $content_user; ?> -->
 									</div>
 									</div>
 									<div class="item-b-r">
+										<!--
 										<a class="link-name-author" href="<?php echo get_author_posts_url($author_id); ?>"><h2><?php echo get_the_author(); ?></h2></a>
 										<div class="img-user">
 											<a href="<?php echo get_author_posts_url($author_id); ?>">
@@ -93,7 +103,9 @@
 												 ?>
 											</a>
 										</div>
+
 										<a class="link-name-author show-600" href="<?php echo get_author_posts_url($author_id); ?>"><h2><?php echo get_the_author(); ?></h2></a>
+										-->
 									</div>
 									
 								</div>
--- wp-content/themes/retailwire/content-braintrust.php
+++ wp-content/themes/retailwire/content-braintrust.php
@@ -1,57 +1,39 @@
-		<?php $num_comment=get_comments_number();
-				              		if($num_comment > 0){ ?>
+								<?php 
+				              		if(get_field('feat_brain')){ ?>
 				              	<div class="item-braintrust">
-
-				              		<?php $args = array(
-									    'number' => 10000,
-									    'post_id' => $post->ID,
-									    'status' => 'approve'
-									);?>
-									<?php $latest_comment = get_comments($args);
-
-
-									if( $latest_comment ) foreach( $latest_comment as $comment ) { 
-										
-										$vote = get_comment_meta( $comment->comment_ID, 'age1', true );
-										//var_dump($vote);
-										if($vote=="on"){
-										?>
-									
 
 									<div class="item-b-l">
 				              			<span class="module-label title-brain">Braintrust</span>
-										<?php $author_id = get_the_author_meta('ID');
-											  $content_user = get_field('content', 'user_'. $author_id );
+										<?php //$author_id = get_the_author_meta('ID');
+											  //$content_user = get_field('content', 'user_'. $author_id );
 											  $username = get_field('feat_brain');
-											  $user = $username['ID'];
+											  $user_id = $username['ID'];
 										 ?>
 									<div class="desc">"<?php the_field('feat_comm'); ?>"</div>
 									</div>
 									<div class="item-b-r">
-										<a class="link-name-author" href="<?php echo get_author_posts_url($author_id); ?>"><h2><?php echo $username["display_name"]; ?></h2></a>
+										<a class="link-name-author" href="<?php echo get_author_posts_url($user_id); ?>"><h2><?php echo $username["display_name"]; ?></h2></a>
 										<div class="img-user">
-											<a href="<?php echo get_author_posts_url($author_id); ?>">
+											<a href="<?php echo get_author_posts_url($user_id); ?>">
 												<?php 
 												 $size="110";
-												 echo get_avatar($user,$size);
+												 echo get_avatar($user_id,$size);
 												 ?>
 											</a>
 										</div>
-										<a class="link-name-author show-600" href="<?php echo get_author_posts_url($author_id); ?>"><h2><?php echo get_the_author(); ?></h2></a>
+										<a class="link-name-author show-600" href="<?php echo get_author_posts_url($user_id); ?>"><h2><?php echo $username["display_name"]; ?></h2></a>
 									</div>
 
-									<?php } ?>
-									<?php } ?>
-				              		
-									
 								</div>
+									
+
 								<?php	} else {  ?>
 
 								<div class="item-braintrust ">
 				              		<div class="item-b-l">
 				              			<span class="module-label title-brain">Braintrust</span>
-										<?php $author_id = get_the_author_meta('ID');
-											  $content_user = get_field('content', 'user_'. $author_id );
+										<?php //$author_id = get_the_author_meta('ID');
+											  //$content_user = get_field('content', 'user_'. $author_id );
 										 ?>
 									<div class="desc"><!--
 									<?php echo $content_user; ?> -->
