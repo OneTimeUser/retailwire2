d942bed42f193992c22c9da8223c860d946e4f5a
OneTimeUser <jason@thewondermob.com>
2016-02-17 11:46:51 -0500

    fixes fields error

--- wp-content/themes/retailwire/page-home.php
+++ wp-content/themes/retailwire/page-home.php
@@ -207,9 +207,9 @@ get_header(); ?>
 				              while ( $wp_query->have_posts() ) : $wp_query->the_post();?>
 				              
 				              <div class="sub-list-press <?php  if ( has_post_thumbnail() == '' ) { echo 'not-thumbnail'; } ?>">
-										<h3><a href="<?php the_field('source_url'); ?>"><?php the_title(); ?></a></h3>
+										<h3><a href="<?php the_field('pr_url'); ?>"><?php the_title(); ?></a></h3>
 										<div class="date-press">
-											<span class="cate_press"><?php the_field('news_source'); ?></span>
+											<span class="cate_press"><?php the_field('pr_source'); ?></span>
 											<span class="full-date-sub"><?php echo get_the_date('m/d/Y'); ?></span>
 										</div>
 				              </div>
