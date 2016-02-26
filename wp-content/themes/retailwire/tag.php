9fd14f5b27000723f82481b1510ee269da4e7f4f
kakenx <info@retailwire.com>
2016-02-26 09:05:02 +0700

    commit 26-02 : tag

--- wp-content/themes/retailwire/tag.php
+++ wp-content/themes/retailwire/tag.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 			<div class="col grid_8_of_12">
 
-				<?php if ( have_posts() ) : ?>
+				<?php //if ( have_posts() ) : ?>
 
 					<header class="archive-header">
 						<h1 class="archive-title"><?php printf( esc_html__( 'Tag Archives: %s', 'quark' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
@@ -23,18 +23,18 @@ get_header(); ?>
 					</header>
 
 					<?php // Start the Loop ?>
-					<?php while ( have_posts() ) : the_post(); ?>
+					<?php
+						$term_id = get_query_var('tag_id');
+						$query = new WP_Query( array( "post_type" => "discussion", "tag_id" => $term_id ) );
+						$count = $query->post_count;
+						if ($count != 0){?>
+					<?php while ($query->have_posts() ) : $query->the_post(); ?>
 						<?php get_template_part( 'content', get_post_format() ); ?>  <!-- 'extract' -->
 					<?php endwhile; ?>
-
-					<?php Retailwire_content_nav( 'nav-below' ); ?>
-
-				<?php else : ?>
-
-					<?php get_template_part( 'no-results' ); // Include the template that displays a message that posts cannot be found ?>
-
-				<?php endif; // end have_posts() check ?>
-
+						<?php Retailwire_content_nav( 'nav-below' ); ?>
+					<?php }else{ ?>
+						<?php get_template_part( 'no-results' ); // Include the template that displays a message that posts cannot be found ?>
+					<?php } ?>
 			</div> <!-- /.col.grid_8_of_12 -->
 			<?php get_sidebar(); ?>
 
