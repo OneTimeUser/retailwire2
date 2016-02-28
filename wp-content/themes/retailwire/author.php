5f6b34b259f5bc51311e4eedbc06c6b1ec92813a
OneTimeUser <jason@thewondermob.com>
2016-02-28 17:57:39 -0500

    fixed pop posts

--- wp-content/themes/retailwire/author.php
+++ wp-content/themes/retailwire/author.php
@@ -53,7 +53,7 @@ get_header(); ?>
 								<div class="group-title-top">
 									<ul class="list-tab-dis">
 										<li class="view-articles"><span class="active" link="#tab2">VIEW ARTICLES</a></li>
-										<li class="view-comment"><span link="#tab1">VIEW COMMENT</a></li>
+										<!-- <li class="view-comment"><span link="#tab1">VIEW COMMENT</a></li> -->
 										
 									</ul>
 
@@ -61,7 +61,6 @@ get_header(); ?>
 								<div class="content-dis">
 									<div class="tabDetails">
 								    	<div id="tab1" class="tabContents">
-								        	
 								        </div>
 								        <div id="tab2" class="tabContents">
 								        	<?php // Start the Loop 
