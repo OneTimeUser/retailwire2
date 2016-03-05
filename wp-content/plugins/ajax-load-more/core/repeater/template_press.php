c76a947b73d8648bfca41ef082a6bd63a9283fe7
OneTimeUser <jason@thewondermob.com>
2016-03-05 13:45:54 -0500

    load more for front page

---
+++ wp-content/plugins/ajax-load-more/core/repeater/template_press.php
@@ -0,0 +1,7 @@
+<div class="sub-list-press <?php  if ( has_post_thumbnail() == '' ) { echo 'not-thumbnail'; } ?>">
+		<h3><a href="<?php the_field('pr_url'); ?>"><?php the_title(); ?></a></h3>
+		<div class="date-press">
+			<span class="cate_press"><?php the_field('pr_source'); ?></span>
+			<span class="full-date-sub"><?php echo get_the_date('m/d/Y'); ?></span>
+		</div>
+</div>
--- wp-content/themes/retailwire/page-home.php
+++ wp-content/themes/retailwire/page-home.php
@@ -196,30 +196,8 @@ get_header(); ?>
 				<div class="home-press">
 					<span class="module-label title-press">PRESS RELEASES</span>
 					<div class="list-press" >
-				    	<?php 
-				          $args2 = array(
-				          	'post_type'=>'press_releases',
-				            'order' => 'desc',
-				            'posts_per_page' => 10
-				          );
+						<?php echo do_shortcode('[ajax_load_more repeater="template_press" post_type="press_releases" posts_per_page="9" scroll="false"]'); ?>
 
-				          $wp_query = new WP_Query( $args2 );
-
-				            if ( $wp_query->have_posts() ) {
-
-				              while ( $wp_query->have_posts() ) : $wp_query->the_post();?>
-				              
-				              <div class="sub-list-press <?php  if ( has_post_thumbnail() == '' ) { echo 'not-thumbnail'; } ?>">
-										<h3><a href="<?php the_field('pr_url'); ?>"><?php the_title(); ?></a></h3>
-										<div class="date-press">
-											<span class="cate_press"><?php the_field('pr_source'); ?></span>
-											<span class="full-date-sub"><?php echo get_the_date('m/d/Y'); ?></span>
-										</div>
-				              </div>
-
-				            <?php endwhile; // end of the loop.
-				            wp_reset_query();
-				            }?>
 
 				    </div>
 
